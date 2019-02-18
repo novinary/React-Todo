fd4fa43ecf9b51b6f3e285f3ba4101f12dc9e090
novinary <novina.dev@gmail.com>
2019-02-18 15:47:39 +0000

    Created todo data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import React from 'react';
 import TodoList from './components/TodoComponents/TodoList';
 import TodoForm from './components/TodoComponents/TodoForm';
 
+//todo array of objects
+const taskList = [
+  {
+    task: 'prep meal for the week',
+    id: 1,
+    completed: false
+  },
+  {
+		task: 'clean kitchen',
+		id: 2,
+		completed: false
+	},
+	{
+		task: 'go to the gym',
+		id: 3,
+		completed: false
+	}
+
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
